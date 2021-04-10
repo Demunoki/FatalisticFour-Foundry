eede82dcbd289ab844fe841f36a8feca1fca594d
Demunoki <82038990+Demunoki@users.noreply.github.com>
2021-04-10 21:27:09 +0200

    Create Intelligence (Medicine).js

---
+++ Macros/Fleshsmith/Intelligence (Medicine).js
@@ -0,0 +1,10 @@
+const actorData = actor ? actor : canvas.tokens.controlled[0];
+const IntMod = actorData.data.data.abilities.int.mod;
+const ProfBonus=2*actorData.data.data.attributes.prof;
+const card = BetterRolls.rollItem(actorData);
+card.addField(["header", { img: actorData.img, title: "Medicine" }]);
+const rollExpr = `1d20+${IntMod}+${ProfBonus}`;
+const title = `Medicine`;
+const rollState = BetterRolls.getRollState();
+card.addField(["check", { formula: rollExpr, rollState, title }]);
+card.toMessage();

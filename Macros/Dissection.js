4e407b3c15ef620770506324f11e7f73f137a416
Demunoki <82038990+Demunoki@users.noreply.github.com>
2021-04-10 20:53:37 +0200

    Create Dissection.js

---
+++ Macros/Dissection.js
@@ -0,0 +1,13 @@
+let actorData = actor ? actor : canvas.tokens.controlled[0];
+let IntMod = actorData.data.data.abilities.int.mod;
+let ProfBonus=2*actorData.data.data.attributes.prof;
+
+const card = BetterRolls.rollItem(actorData);
+card.addField(["header", { img: actorData.img, title: "Dissection" }]);
+let rollExpr = `1d20+${IntMod}+${ProfBonus}`;
+let damage = `1d4+${IntMod}`;
+const title = `Dissection`;
+const rollState = BetterRolls.getRollState();
+card.addField(["check", { formula: rollExpr, rollState, title }]);
+card.addField(["check", { formula: `${damage}` }]);
+card.toMessage();

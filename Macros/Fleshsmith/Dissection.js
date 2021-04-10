1e82b6203f84fca15f86d82836fc9c8b622725c5
Demunoki <82038990+Demunoki@users.noreply.github.com>
2021-04-10 21:27:49 +0200

    Update Dissection.js

--- Macros/Fleshsmith/Dissection.js
+++ Macros/Fleshsmith/Dissection.js
@@ -1,13 +1,13 @@
-const actorData = actor ? actor : canvas.tokens.controlled[0];
-const IntMod = actorData.data.data.abilities.int.mod;
-const ProfBonus=2*actorData.data.data.attributes.prof;
+let actorData = actor ? actor : canvas.tokens.controlled[0];
+let IntMod = actorData.data.data.abilities.int.mod;
+let ProfBonus=2*actorData.data.data.attributes.prof;
 
 const card = BetterRolls.rollItem(actorData);
 card.addField(["header", { img: actorData.img, title: "Dissection" }]);
-const rollExpr = `1d20+${IntMod}+${ProfBonus}`;
-const damage = `1d4+${IntMod}`;
+let rollExpr = `1d20+${IntMod}+${ProfBonus}`;
+let damage = `1d4+${IntMod}`;
 const title = `Dissection`;
 const rollState = BetterRolls.getRollState();
-card.addField(["check", { formula: rollExpr, rollState, title }]);
-card.addField(["check", { formula: `${damage}` }]);
+card.addField(["attack", { formula: rollExpr, rollState, title }]);
+card.addField(["damage", { formula: `${damage}` }]);
 card.toMessage();

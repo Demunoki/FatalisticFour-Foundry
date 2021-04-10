12465d02ccfb4e79569cbb98729486b0dd04f8f5
Demunoki <82038990+Demunoki@users.noreply.github.com>
2021-04-10 21:43:20 +0200

    Update Remove Hit Die.js

--- Macros/Fleshsmith/Remove Hit Die.js
+++ Macros/Fleshsmith/Remove Hit Die.js
@@ -1,8 +1,10 @@
 const actorData = actor ? actor : canvas.tokens.controlled[0];
 if(actorData == null) 
     ui.notifications.warn(`No token selected`);
+else{
 actorData.data.data.attributes.hd--;
   ChatMessage.create({
                             speaker: ChatMessage.getSpeaker(),
                             content: `${actorData.name} spent 1 Hitdice`
                         });
+}

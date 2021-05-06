81e8a330a54204dedc466fc7808f7520198e30b4
Demunoki <82038990+Demunoki@users.noreply.github.com>
2021-05-06 18:20:19 +0200

    Update Reroll Rifle.js

--- Macros/Reroll Rifle.js
+++ Macros/Reroll Rifle.js
@@ -1,10 +1,13 @@
 let actorData = actor ? actor : canvas.tokens.controlled[0];
-let Musket = actorData ? actorData.items.find(i => i.name==="Musket") : null
+let Musket = actorData ? actorData.data.items.find(i => i.name==="Musket") : null;
+let mod = actorData.data.data.abilities.dex.mod;
 if(actorData == null|| Musket == null ) 
     ui.notifications.warn(`I have a bad feeling about this`);
   else{
-  let damage=Musket.data.damage.parts.0[0];
-  
-  const card = BetterRolls.rollItem(actorData);
-     card.addField(["damage", { formula:`${damage}` }]);
-     card.toMessage();}
+    let damage=Musket.data.damage.parts[0][0];
+damage=damage.replace('@mod', mod);
+console.log(damage);
+ const card = BetterRolls.rollItem(actorData);
+     card.addField(["damage", { formula:damage}]);
+     card.toMessage();
+}
